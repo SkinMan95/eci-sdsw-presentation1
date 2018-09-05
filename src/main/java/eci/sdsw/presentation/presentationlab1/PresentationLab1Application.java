fbc6792252fea141dca694c7e8c0409684d956db
Alejandro Anzola Avila <anzola95avila@gmail.com>
2018-09-05 10:29:36 -0500

    seguridad funcional

--- src/main/java/eci/sdsw/presentation/presentationlab1/PresentationLab1Application.java
+++ src/main/java/eci/sdsw/presentation/presentationlab1/PresentationLab1Application.java
@@ -2,8 +2,9 @@ package eci.sdsw.presentation.presentationlab1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class PresentationLab1Application {
 
 	public static void main(String[] args) {
