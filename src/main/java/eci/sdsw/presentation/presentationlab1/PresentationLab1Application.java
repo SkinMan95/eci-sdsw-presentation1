44692223dfd4f8542b7e0450ad04e347ab9e7cd2
Alejandro Anzola <anzola95avila@gmail.com>
2018-09-04 21:37:43 -0500

    commit inicial

---
+++ src/main/java/eci/sdsw/presentation/presentationlab1/PresentationLab1Application.java
@@ -0,0 +1,12 @@
+package eci.sdsw.presentation.presentationlab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentationLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresentationLab1Application.class, args);
+	}
+}
